b8b76aaa906181cb464d2a442baa338db79b1010
adrian gallero <agallero@gmail.com>
2018-01-16 06:21:29 -0300

    Examples for FlexCel 6.18.5

--- csharp/VS2012/netframework/Modules/25.Printing and Exporting/35.Signing Pdfs/Properties/AssemblyInfo.cs
+++ csharp/VS2012/netframework/Modules/25.Printing and Exporting/35.Signing Pdfs/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Signing Pdfs")]
-[assembly: AssemblyCopyright("(c) 2002 - 2017 TMS Software")]
+[assembly: AssemblyCopyright("(c) 2002 - 2018 TMS Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.18.0.0")]
-[assembly: AssemblyFileVersion("6.18.0.0")]
+[assembly: AssemblyVersion("6.18.5.0")]
+[assembly: AssemblyFileVersion("6.18.5.0")]
--- csharp/VS2017/winrt81/LangWars/LangWars.Windows/Properties/AssemblyInfo.cs
+++ csharp/VS2017/winrt81/LangWars/LangWars.Windows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LangWars.Windows")]
-[assembly: AssemblyCopyright("(c) 2002 - 2017 TMS Software")]
+[assembly: AssemblyCopyright("(c) 2002 - 2018 TMS Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.18.0.0")]
-[assembly: AssemblyFileVersion("6.18.0.0")]
+[assembly: AssemblyVersion("6.18.5.0")]
+[assembly: AssemblyFileVersion("6.18.5.0")]
 [assembly: ComVisible(false)]
