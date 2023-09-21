533db4d6e7a3184ba05e95320bf8ee74acda5277
adrian gallero <agallero@gmail.com>
2023-09-21 10:21:38 -0300

    Examples for FlexCel 7.19

--- csharp/VS2017/netframework/Modules/25.Printing and Exporting/35.Signing Pdfs/Properties/AssemblyInfo.cs
+++ csharp/VS2017/netframework/Modules/25.Printing and Exporting/35.Signing Pdfs/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.18.0.0")]
-[assembly: AssemblyFileVersion("7.18.0.0")]
+[assembly: AssemblyVersion("7.19.0.0")]
+[assembly: AssemblyFileVersion("7.19.0.0")]
